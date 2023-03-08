72eea1223aaf1c993b77652710901c628c832e1f
Dynasty016 <gustavobrasil250@hotmail.com>
2023-03-08 16:06:08 -0300

    Criei as Classes "Cliente" e "Endereco" e criei os metodos get e set em ambas

---
+++ ConsoleApp_Tarde/Endereco.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Tarde
+{
+    internal class Endereco
+    {
+        private int Id;
+        private string Rua;
+        private string Bairro;
+        private string Cidade;
+        private int Numero;
+        public string CEP;
+
+
+        public Endereco(int id, string rua, string bairro, string cidade, int numero, string cep) 
+        {
+            Id = id;
+            Rua = rua;
+            Bairro = bairro;
+            Cidade = cidade;
+            Numero = numero;
+            CEP = cep;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public string getRua()
+        {
+            return Rua;
+        }
+
+        public void setRua(string rua)
+        {
+            Rua = rua;
+        }
+
+        public string getBairro()
+        {
+            return Bairro;
+        }
+
+        public void setBairro(string bairro)
+        {
+            Bairro = bairro;
+        }
+
+        public string getCidade()
+        {
+            return Cidade;
+        }
+
+        public void setCidade(string cidade) 
+        {
+            Cidade = cidade;
+        }
+
+        public int getNumero()
+        {
+            return Numero;
+        }
+
+        public string getCEP()
+        {
+            return CEP;
+        }
+
+        public void setCEP(string cep)
+        {
+            CEP = cep;
+        }
+    }
+}

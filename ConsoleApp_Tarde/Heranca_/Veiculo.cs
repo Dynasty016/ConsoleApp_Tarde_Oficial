628b77373e72537991b7d0dd793c376696682cfb
Dynasty016 <gustavobrasil250@hotmail.com>
2023-03-15 15:29:42 -0300

    Adicionei a pasta "Heranca" e criei as classes dentro, e tambem a variavel List

---
+++ ConsoleApp_Tarde/Heranca_/Veiculo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Tarde.Heranca2
+{
+    public class Veiculo
+    {
+        public string cor;
+        public int quantidadedeRodas;
+        public int quantidadedeAssentos;
+        public bool TemMotor;
+
+        public string ExibirDados()
+        {
+            if (TemMotor)
+            {
+                return "Tem motor" + cor + " " + quantidadedeAssentos + " " + quantidadedeRodas;
+            }
+
+            else
+            {
+                return cor + " " + quantidadedeAssentos + " " + quantidadedeRodas;
+            }
+        }
+    }
+}
--- ConsoleApp_Tarde/Program.cs
+++ ConsoleApp_Tarde/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp_Tarde;
-
-
+using ConsoleApp_Tarde.Heranca2;
+using Microsoft.VisualBasic;
 
 Produto p1 = new Produto(1,"Mouse xing ling", 10.90M);
 Produto p2 = new Produto(2,"Teclado multilaser", 20.90M);
@@ -13,15 +13,13 @@ p1.setDescricao("Mouse Razer");
 
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
 
-Endereco e1 = new Endereco(1,
-    "15906304",
-    "Mario Miziara",
-    91,
-    "Sobral",
-    "Taquaritinga"
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+Endereco e1 = new Endereco(1, "15906304", "Mario Miziara", 91, "Sobral", "Taquaritinga"
     );
 
-Cliente c1 = new Cliente(1, "Fernando", "991340447",e1);
+Cliente c1 = new Cliente(1, "Gustavo", "991340447",e1);
 Cliente c2 = new Cliente(2, "Victoria", "991340447",e1);
 Cliente c3 = new Cliente(3, "Rafael", "991340447", e1);
 Cliente c4 = new Cliente(4, "Lucas", "9913409876" ,e1  );
@@ -34,8 +32,35 @@ Console.WriteLine(c1.getEndereco().EnderecoCompleto() );
 c1.getEndereco().AtualizarEndereco("Julio de Moraes", 585, "POrtal Itamaraca");
 Console.WriteLine(c1.getEndereco().EnderecoCompleto());
 
+Console.WriteLine(" ");
+Console.WriteLine(" ");
 
+Carro car = new Carro();
+car.cor = "vermelho";
+car.quantidadedeRodas = 4;
+car.quantidadedeAssentos = 5;
+car.TemMotor = true;
 
+Bike bicicleta = new Bike();
+bicicleta.cor = "Azul";
+bicicleta.quantidadedeRodas = 2;
+bicicleta.quantidadedeAssentos = 1;
+bicicleta.TemMotor = false;
 
+Aviao aviao = new Aviao();
+aviao.quantidadedeRodas = 12;
+aviao.quantidadedeAssentos = 500;
+aviao.cor = "Branco";
+aviao.TemMotor = true;
 
+List<Veiculo> veiculoLista = new List <Veiculo>();
+veiculoLista.Add(car);
+veiculoLista.Add(aviao);
+veiculoLista.Add(bicicleta);
 
+foreach (var item in veiculoLista)
+{
+    Console.WriteLine(item.ExibirDados());
+}
+
+Console.WriteLine("Qtd itens da Lista" +veiculoLista.Count);
